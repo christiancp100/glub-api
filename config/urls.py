4acc8cce164094c3614c25d3eacbbc252abeff60
Christian Capeans <christiancp100@gmail.com>
2021-11-17 22:27:37 +0100

    Add payment intents

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('api/', include('apps.accounts.urls')),
     path('api/', include('apps.bars.urls')),
     path('api/', include('apps.events.urls')),
+    path('api/', include('apps.tickets.urls')),
     path('api/registry/', include('apps.registry.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
